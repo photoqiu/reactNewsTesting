43f2e40af76aa3b0da5885337cd5d0d3b9c14307
ext.qiubo <ext.qiubo@jd.com>
2021-06-03 19:13:21 +0800

    feat: ReactJS change UI.

--- src/pages/Draw/DrawLayerout.tsx
+++ src/pages/Draw/DrawLayerout.tsx
@@ -1,28 +1,35 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+/*
+ * @Description:
+ * @Author: ext.qiubo
+ * @Date: 2021-06-03 09:30:13
+ * @LastEditors: ext.qiubo
+ * @LastEditTime: 2021-06-03 11:53:30
+ */
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 
-function Box(props:number[] | any) {
+function Box(props: number[] | any) {
     // This reference will give us direct access to the mesh
-    const mesh:any = useRef()
+    const mesh: any = useRef();
     // Set up state for the hovered and active state
-    const [hovered, setHover] = useState<boolean>(false)
-    const [active, setActive] = useState<boolean>(false)
+    const [hovered, setHover] = useState<boolean>(false);
+    const [active, setActive] = useState<boolean>(false);
     // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame(() => ( mesh.current.rotation.x += 0.01 ))
+    useFrame(() => (mesh.current.rotation.x += 0.01));
     // Return view, these are regular three.js elements expressed in JSX
     return (
-      <mesh
-        {...props}
-        ref={mesh}
-        scale={active ? 1.5 : 1}
-        onClick={() => setActive(!active)}
-        onPointerOver={() => setHover(true)}
-        onPointerOut={() => setHover(false)}
-      >
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    )
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? 1.5 : 1}
+            onClick={() => setActive(!active)}
+            onPointerOver={() => setHover(true)}
+            onPointerOut={() => setHover(false)}
+        >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    );
 }
 
 const DrawLayerOut: React.FC = () => {
@@ -32,9 +39,17 @@ const DrawLayerOut: React.FC = () => {
             <pointLight position={[10, 10, 10]} />
             <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} />
+            <mesh
+                visible
+                userData={{ hello: 'world' }}
+                position={[1, 0, 0]}
+                rotation={[Math.PI / 2, 0, 0]}
+            >
+                <sphereGeometry args={[1, 26, 26]} />
+                <meshStandardMaterial color="orange" transparent />
+            </mesh>
         </Canvas>
-    )
-}
+    );
+};
 
 export default DrawLayerOut;
-  
